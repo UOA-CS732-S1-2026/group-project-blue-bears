559c9883c8f93b679b374015d44b85216a564959
ljun123@aucklanduni.ac.nz <ljun123@aucklanduni.ac.nz>
2026-04-07 14:13:20 +1200

    routing for result page with static props

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,15 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import LoginPage from './pages/LoginPage'
 import RegisterPage from './pages/RegisterPage'
+import ResultPage from './pages/ResultPage'
+
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<div>Landing Page</div>} />
+        
+        <Route path="/" element={<ResultPage outcome="victory" playerStats={{ wpm: 120, accuracy: 95 }} opponentStats={{ wpm: 110, accuracy: 85 }} duration="0:21" onPlayAgain={() => {}} onMainMenu={() => {}} />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
       </Routes>
